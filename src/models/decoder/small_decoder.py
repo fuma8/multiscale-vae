857d9ecd27d7582088278460857be813890ad298
fuma8 <fuma.kimishima.3c@stu.hosei.ac.jp>
2025-05-10 14:48:10 +0900

    first commit

--- src/models/decoder/small_decoder.py
+++ src/models/decoder/small_decoder.py
@@ -1,11 +1,11 @@
 import torch.nn as nn
 
-class SmallEncoder(nn.Module):
+class SmallDecoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=4, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=4, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=3, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU()
     
     def forward(self, x):
