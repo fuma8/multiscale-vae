a01fc5bddb6fc73708bd8dc4c7b62c90fd4045f0
fuma8 <fuma.kimishima.3c@stu.hosei.ac.jp>
2025-06-02 15:30:31 +0900

    first commit

--- src/models/decoder/small_decoder.py
+++ src/models/decoder/small_decoder.py
@@ -4,11 +4,11 @@ from src.registry.registry import GLOBAL_REGISTRY
 
 @GLOBAL_REGISTRY.register(category='decoder', name='SmallDecoder')
 class SmallDecoder(nn.Module):
-    def __init__(self, latent_channels, out_channels):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.latent_channels = latent_channels
+        self.in_channels = in_channels
         self.out_channels = out_channels
-        self.conv1 = nn.Conv2d(in_channels=self.latent_channels, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=self.in_channels, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=self.out_channels, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU()
--- src/models/encoder/small_encoder.py
+++ src/models/encoder/small_encoder.py
@@ -4,13 +4,13 @@ from src.registry.registry import GLOBAL_REGISTRY
 
 @GLOBAL_REGISTRY.register(category='encoder', name='SmallEncoder')
 class SmallEncoder(nn.Module):
-    def __init__(self, in_channels, latent_channels):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
         self.in_channels = in_channels
-        self.latent_channels = latent_channels
+        self.out_channels = out_channels
         self.conv1 = nn.Conv2d(in_channels=self.in_channels, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=self.latent_channels*2, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=self.out_channels*2, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU()
     
     def forward(self, x):
